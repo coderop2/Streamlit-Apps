84922da42901e6de79f8c970e8b10c8e80e04510
coderop2 <harshitkhandelwal305@gmail.com>
2021-01-25 04:58:20 +0530

    Streamlit UI

--- QnABot/code/start.py
+++ QnABot/code/start.py
@@ -16,31 +16,41 @@ def loadData():
                 for line in f.readlines():
                     if(len(line.strip()) > 0):
                         docs[name]['data'].append(line.strip())
-
-            docs[name]['contextobj'] = PC(docs[name]['data'])
         return docs
     except FileNotFoundError:
-        print("Bot> Oops! Currently i dont have \"" + databasename + "\""+" in my database set")
         exit()
+        
+def get_file_content_as_string(file):
+    return open('../dataset/'+file, 'r', encoding = 'utf-8').read()
+
+# def processContextWithNewParams(docs, param1, param2, param3, param4):
+#     for i in docs:
+#         docs[name]['contextobj'] = PC(docs[name]['data'])
+#     return docs
 
 docs = loadData()
 instead_use_lemmanization = False
 possible_db_name = os.listdir('../dataset')
 stemmer = "No Stemmer"
+lemm_stemm_flag = "No"
+specific = False
+ut = UT()
 yesnoDict = {
              "Yes" : True,
              "No"  : False
             }
 
 st.sidebar.title("Configure Your Bot")
-lemm_or_stemm = st.sidebar.selectbox("Stemming or Lemmanization of the word", ["Stemming", "Lemmanization"])
+lemm_or_stemm = st.sidebar.selectbox("Stemming or Lemmanization of the word", ["None","Stemming", "Lemmanization"])
 
 if lemm_or_stemm == "Lemmanization":
     title_header = "Choose lemmanization option"
-else:
+    lemm_stemm_flag = st.sidebar.selectbox(title_header, ["Yes", "No"])
+else if lemm_or_stemm == "Stemming":
     title_header = "Choose Stemming option"
+    lemm_stemm_flag = st.sidebar.selectbox(title_header, ["Yes", "No"])
 
-lemm_stemm_flag = st.sidebar.selectbox(title_header, ["Yes", "No"])
+
 
 if lemm_or_stemm == "Stemming" and lemm_stemm_flag == "Yes":
     stemmer = st.sidebar.selectbox("Choose which stemmer", ["PorterStemmer", "SnowBallStemmer"])
@@ -51,40 +61,40 @@ use_synonyms_flag = st.sidebar.selectbox("Use Synonyms", ["Yes", "No"])
 similarity_func = st.sidebar.selectbox("Which Sentence level similarity function to use", ["SkLearn", "Gensim", "User Made"])
 show_file = st.sidebar.checkbox("Show Selected File")
 
-
-st.title("Hi There !! I am a Q and A bot !!!")
+st.title("Hi There !!")
+st.header("I am a Q and A bot !!!")
 st.markdown("I can answer many types of question belonging to any of the category "
                 "shown in the List below ")
 
-st.selectbox("Choose the Data from which you want to ask questions from", possible_db_name)
+file_selected = st.selectbox("Choose the Data from which you want to ask questions from", possible_db_name)
+if show_file:
+    st.markdown(get_file_content_as_string(file_selected))
 
+placeholder = "Enter your Query here ..."
+user_query = st.text_input("User Input", placeholder)
 
-specific = False
-ut = UT()
+# docs = processContextWithNewParams(docs, removestopword_flag, use_synonyms_flag, lemm_or_stemm, lemm_stemm_flag, stemmer, similarity_func)
+specificData = PC(docs[file_selected]['data'], yesnoDict[removestopword_flag], lemm_or_stemm, yesnoDict[lemm_stemm_flag], stemmer, similarity_func)
 
+greetPattern = re.compile("^\ *((hey)|(hi+)|((good\ )?morning|evening|afternoon)|(he((llo)|y+)))\ *$",re.I)
+exitPattern = re.compile("^\ *((bye*\ ?)|(see (you|ya) later))\ *$",re.I)
 
-
-while True:
+if user_query != placeholder:
+    
     response = ""
-    userInp = input("You-> ")
-    if not len(userInp) > 5:
+    if not len(user_query) > 5:
         response = "Please enter valid question."
-    elif greetPattern.match(userInp):
+    elif greetPattern.match(user_query):
         response = "Hello!"
-    elif exitPattern.match(userInp):
+    elif exitPattern.match(user_query):
         response = "See ya next time!"
         print("Bot-> ", response)
         break
     else:
         if specific:
-            objPQ = PQ(userInp)
-            # print(len(specificData.paraInfo))
+            objPQ = PQ(user_query, yesnoDict[removestopword_flag], yesnoDict[use_synonyms_flag])
             response = specificData.getResults(objPQ)
-        else:
-            closestvector, obj = ut.GetClosestContextFile(1)
-            if closestvector == 0:
-                response = "Please provide a better question with more context"
-            else:
-                response = "Going wide"
-    print("Bot-> ", response)
+
+            # closestvector, obj = ut.GetClosestContextFile(1)
+    st.write(response)
     
--- QnABot/code/test.py
+++ QnABot/code/test.py
@@ -1,7 +1,14 @@
 import streamlit as st
 import os
 
+@st.cache(show_spinner=True)
+def get_file_content_as_string(file):
+    return open('../dataset/'+file,'r', encoding='utf-8').read()
+
+# docs = loadData()
+instead_use_lemmanization = False
 possible_db_name = os.listdir('../dataset')
+stemmer = "No Stemmer"
 yesnoDict = {
              "Yes" : True,
              "No"  : False
@@ -9,25 +16,30 @@ yesnoDict = {
 
 st.sidebar.title("Configure Your Bot")
 lemm_or_stemm = st.sidebar.selectbox("Stemming or Lemmanization of the word", ["Stemming", "Lemmanization"])
+
 if lemm_or_stemm == "Lemmanization":
     title_header = "Choose lemmanization option"
 else:
     title_header = "Choose Stemming option"
+
 lemm_stemm_flag = st.sidebar.selectbox(title_header, ["Yes", "No"])
+
 if lemm_or_stemm == "Stemming" and lemm_stemm_flag == "Yes":
     stemmer = st.sidebar.selectbox("Choose which stemmer", ["PorterStemmer", "SnowBallStemmer"])
     
 removestopword_flag = st.sidebar.selectbox("Remove Stopwords", ["Yes", "No"])
 use_synonyms_flag = st.sidebar.selectbox("Use Synonyms", ["Yes", "No"])
-similarity_func = st.sidebar.selectbox("Which Sentence level similarity function to use", ["SKlearn", "Gensim", "User Made"])
+
+similarity_func = st.sidebar.selectbox("Which Sentence level similarity function to use", ["SkLearn", "Gensim", "User Made"])
 show_file = st.sidebar.checkbox("Show Selected File")
-st.write(show_file)
-st.title("Consolidated DashBoard since the OutBreak")
-st.markdown("This application is a Streamlit dashboard that can be used "
-                "to analyze the spread of COVID-19 💥🚗")
 
-st.selectbox("Choose the Data from which you want to ask questions from", possible_db_name)
+st.title("Hi There !! I am a Q and A bot !!!")
+st.markdown("I can answer many types of question belonging to any of the category "
+                "shown in the List below ")
 
+file_selected = st.selectbox("Choose the Data from which you want to ask questions from", possible_db_name)
+if show_file:
+    st.markdown(get_file_content_as_string(file_selected))
 
 value = "default value"
 # if st.button('reset textarea'):
